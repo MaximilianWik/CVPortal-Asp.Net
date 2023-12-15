60e13d3b5dde6c60eb1f720901cf48e61b3d9b0a
MaximilianWik <132461608+MaximilianWik@users.noreply.github.com>
2023-12-15 15:53:47 +0100

    AnvändareRegisterViewModel MJAUUUUUU

---
+++ CVPortalen/Models/AnvandareRegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CVPortalen.Models
+{
+    public class AnvandarRegisterViewModel
+    {
+
+        [Required(ErrorMessage = "Vänligen skriv ett användarnamn din feta slyna.")]
+        [StringLength(255)]
+        public string AnvandarNamn { get; set; }
+
+        [Required(ErrorMessage = "Vänligen skriv lösenord horunge.")]
+        [DataType(DataType.Password)]
+        [Compare("BekraftaLosenord")]
+        public string Losenord { get; set; }
+
+        [Required(ErrorMessage = "Vänlingen bekräfta lösenordet apjävel")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Bekrafta losenordet")]
+        public string BekraftaLosenord { get; set; }
+    }
+}
