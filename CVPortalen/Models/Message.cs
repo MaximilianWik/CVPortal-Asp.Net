f6b8be3a245c895ace768282c2ac198334e0774a
MaximilianWik <132461608+MaximilianWik@users.noreply.github.com>
2023-12-21 16:26:47 +0100

    Message model

---
+++ CVPortalen/Models/Message.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CVPortalen.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string SenderId { get; set; }
+
+        [Required]
+        public string ReceiverId { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public DateTime SentAt { get; set; }
+
+        public bool IsRead { get; set; }
+
+        // Lägg till detta till navigation property
+        public Anvandare Sender { get; set; }
+        public Anvandare Receiver { get; set; }
+    }
+
+}
