e1523b204c2a1699cd0845e9bee53a36efc0183b
jackeenorm123 <jackewahlstrm1@gmail.com>
2024-01-07 14:33:04 +0100

    miggar

---
+++ CVPortalen/Migrations/20240107124733_testar.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CVPortalen.Migrations
+{
+    /// <inheritdoc />
+    public partial class testar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_SenderId",
+                table: "Messages");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_SenderId",
+                table: "Messages",
+                column: "SenderId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_AspNetUsers_SenderId",
+                table: "Messages");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_AspNetUsers_SenderId",
+                table: "Messages",
+                column: "SenderId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
